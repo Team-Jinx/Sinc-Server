426eef283290003da902d021ae3ebe2bf57880c8
YeongWoooo <rdd9223@gmail.com>
2021-12-04 04:34:24 +0900

    Refactor: 사용하지 않는 라이브러리 제거

--- server/src/stories/models/story-with-performance-statistics.model.ts
+++ server/src/stories/models/story-with-performance-statistics.model.ts
@@ -1,12 +1,31 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { PerformanceModel } from 'src/performances';
+import { NotificationModel } from 'src/shared/notification';
+import { UsersCheeredPerformancesModel } from 'src/users-cheered-performances';
 
 import { StoryModel } from './story.model';
+import { StoryType } from '.prisma/client';
 
 @ObjectType()
-export class StoryWithPerformanceStatisticsModel extends StoryModel {
+export class StoryWithPerformanceStatisticsModel implements StoryModel {
   @Field(() => Int)
   public amount?: number;
 
   @Field(() => Int)
   public ticketCount?: number;
+
+  @Field(() => StoryType, { nullable: true })
+  public type!: StoryType | null;
+
+  public id!: string;
+  public cheerCount!: number | null;
+  public performanceId!: string;
+  public imageUrl!: string | null;
+  public videoUrl!: string | null;
+  public description!: string;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+  public performance?: PerformanceModel;
+  public usersCheeredPerformances?: UsersCheeredPerformancesModel[];
+  public notifications?: NotificationModel[];
 }
